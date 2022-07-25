fee58c6e30bface3f5dfa6626fd4be351bc9b5bf
Samurai-way <olegmoishevych@gmail.com>
2022-07-25 21:43:07 +0300

    Lesson2

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {TodoList} from "./TodoList";
+import {TaskType, Todolist} from "./Todolist";
+
+export type FilterValueType = 'all' | 'active' | 'completed'
+
 
 function App() {
 
-    let tasks1 = [
-        {id: 1, title: 'CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false},
-    ]
+    let [tasks, setTasks] = useState<Array<TaskType>>([
+        { id: 1, title: "CSS", isDone: false },
+        { id: 2, title: "JS", isDone: true },
+        { id: 3, title: "ReactJS", isDone: false },
+    ])
 
-    let tasks2 = [
-        {id: 1, title: 'Terminator', isDone: true},
-        {id: 2, title: 'XXX', isDone: false},
-        {id: 3, title: 'Jentelmens of fortune', isDone: false},
-    ]
+    const [filter, setFilter] = useState<FilterValueType>('all')
+    const removeTask = (taskID: number) => {
+        setTasks(tasks.filter(task => task.id !== taskID))
+    }
+
+    const changeFilter = (filter: FilterValueType) =>{
+        setFilter(filter)
+    }
+    let tasksForRender;
+    switch (filter) {
+        case "completed":
+            tasksForRender = tasks.filter(tasks=>tasks.isDone)
+            break
+        case "active":
+            tasksForRender = tasks.filter(tasks=>!tasks.isDone)
+            break
+        default:
+            tasksForRender = tasks
+    }
 
     return (
         <div className="App">
-            <TodoList title={"What to learn"} tasks={tasks1}/>
-            <TodoList title={"Songs"} tasks={tasks2}/>
+            <Todolist title="What to learn"
+                      tasks={tasksForRender}
+                      removeTask={removeTask}
+                      changeFilter={changeFilter}
+            />
         </div>
     );
 }
 
 export default App;
-
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React from 'react';
+import {FilterValueType} from "./App";
 
-type TaskType = {
+export type TaskType = {
     id: number
     title: string
     isDone: boolean
@@ -9,26 +10,34 @@ type TaskType = {
 type PropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: (taskID: number) => void
+    changeFilter: (filter: FilterValueType)=>void
 }
 
-export function TodoList(props: PropsType) {
-    return (
+export function Todolist(props: PropsType) {
+
+    let tasksItems = props.tasks.map((task: TaskType)=>{
+        return <li key={task.id}>
+            <input type="checkbox" checked={props.tasks[0].isDone}/>
+            <span>{props.tasks[0].title}</span>
+            <button onClick={()=>props.removeTask(task.id)
+            }>XXL</button>
+        </li>
+    })
+
+    return <div>
+        <h3>{props.title}</h3>
         <div>
-            <h3>{props.title}</h3>
-            <div>
-                <input/>
-                <button>+</button>
-            </div>
-            <ul>
-                <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
-            </ul>
-            <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
-            </div>
+            <input/>
+            <button>+</button>
         </div>
-    )
+        <ul>
+            {tasksItems}
+        </ul>
+        <div>
+            <button onClick={()=>props.changeFilter('all')}>All</button>
+            <button onClick={()=>props.changeFilter('active')}>Active</button>
+            <button onClick={()=>props.changeFilter('completed')}>Completed</button>
+        </div>
+    </div>
 }
