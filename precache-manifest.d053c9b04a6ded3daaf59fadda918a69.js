d2ba9ce8c5b418444e54696c9b522e3c98a51708
Samurai-way <olegmoishevych@gmail.com>
2022-08-03 19:56:53 +0300

    Updates

---
+++ precache-manifest.d053c9b04a6ded3daaf59fadda918a69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369a9cb0a59019082bd12dd1e8f17877",
+    "url": "/React_lessons/index.html"
+  },
+  {
+    "revision": "7399174a121748289fd1",
+    "url": "/React_lessons/static/css/main.6f30ddfc.chunk.css"
+  },
+  {
+    "revision": "e597f10f6272895e704b",
+    "url": "/React_lessons/static/js/2.66ffb61e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React_lessons/static/js/2.66ffb61e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7399174a121748289fd1",
+    "url": "/React_lessons/static/js/main.5d4dafc7.chunk.js"
+  },
+  {
+    "revision": "2b3f9c6b17bc84629b65",
+    "url": "/React_lessons/static/js/runtime-main.ab87335c.js"
+  }
+]);
