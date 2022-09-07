22b1ccba3ab646c73797aa20440e7a10bb82d162
Samurai-way <olegmoishevych@gmail.com>
2022-09-07 11:29:02 +0300

    Updates

---
+++ precache-manifest.37fe5e712287451b410bfd4300c8658d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06eadde20b8d8d6ddcd74f5d627d9a95",
+    "url": "/React_lessons/index.html"
+  },
+  {
+    "revision": "a78e79e06c6c2992cf94",
+    "url": "/React_lessons/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "f4111f4d560db2ef5bbe",
+    "url": "/React_lessons/static/js/2.ab0d77dd.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/React_lessons/static/js/2.ab0d77dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78e79e06c6c2992cf94",
+    "url": "/React_lessons/static/js/main.52d5d80f.chunk.js"
+  },
+  {
+    "revision": "2b3f9c6b17bc84629b65",
+    "url": "/React_lessons/static/js/runtime-main.ab87335c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React_lessons/precache-manifest.07aa9051694322dbd750274f99e38704.js"
+  "/React_lessons/precache-manifest.37fe5e712287451b410bfd4300c8658d.js"
 );
 
 self.addEventListener('message', (event) => {
